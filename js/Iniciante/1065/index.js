78e7d92925444f8d528a377560915898f13325ee
Gabriel Souza <gabrielsouzades@gmail.com>
2022-10-03 18:17:32 -0300

    resolution of exercise 1065

---
+++ js/Iniciante/1065/index.js
@@ -0,0 +1,13 @@
+var input = require('fs').readFileSync('index', 'utf8');
+var line = input.split('\n')
+
+var count = 0
+
+line.forEach(t => {
+    if(t % 2 === 0){
+        count++
+    }
+})
+
+
+console.log(count + ' valores pares ')
